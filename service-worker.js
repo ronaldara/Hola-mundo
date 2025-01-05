42f4546c3f90215bb8c0eaad2d8ca3537068153f
ronaldara <123225285+ronaldara@users.noreply.github.com>
2025-01-05 16:25:59 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,10 +6,10 @@ const urlsToCache = [
 ];
 
 // Instalación del Service Worker
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         console.log('Archivos en caché');
         return cache.addAll(urlsToCache);
       })
@@ -17,27 +17,39 @@ self.addEventListener('install', event => {
 });
 
 // Activación del Service Worker
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cache => {
+        cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
             console.log('Eliminando caché antigua:', cache);
-            return caches.delete(cache);
+            return caches.delete(cache); // Eliminar cachés antiguas
           }
         })
       );
+    }).then(() => {
+      // Cambiar el controlador de clientes para que los clientes carguen el nuevo SW
+      return self.clients.claim();
     })
   );
 });
 
 // Interceptar solicitudes
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
+      .then((response) => {
+        // Si la respuesta está en caché, se retorna desde ahí. Si no, se hace una petición de red
+        return response || fetch(event.request).then((networkResponse) => {
+          // Si la respuesta es exitosa, se agrega al caché para futuras solicitudes
+          if (networkResponse && networkResponse.status === 200) {
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+            });
+          }
+          return networkResponse;
+        });
       })
   );
 });
